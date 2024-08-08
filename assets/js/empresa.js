d752ff2033940b5605712fb2934070de69c84783
ivanr978 <87047013+ivanr978@users.noreply.github.com>
2024-08-08 13:21:22 -0400

    agregue al constructor de empresa importaciones y el metodo para calcular el total de importaciones

--- assets/js/empresa.js
+++ assets/js/empresa.js
@@ -2,10 +2,11 @@ export class Empresa{
 constructor(idEmpresa, rut, nombre) {
     this.idEmpresa = idEmpresa;
     this.rut = rut;
-}
+    this.importaciones = []; // Lista de importaciones realizadas por la empresa
+    }
 }
 agregarImportaciones(Importaciones) {
-    
+    this.importaciones.push(importacion);  
 }
 
 export class Importacion extends Empresa {
@@ -17,15 +18,12 @@ constructor (idProducto, nombreProducto, numeroProducto, precioProducto ){
     this.precioProducto = precioProducto;
 }
 
-calcularPrecioindividual {
+calcularPrecioindividual() {
     return this.numeroProducto * this.precioProducto;
 }
 
 
-
-calcularTotalImportaciones () {
-    
 }
-
-
+calcularTotalImportaciones() {
+    return this.importaciones.reduce((total, importaciones) => total + importaciones.calcularPrecioindividual, 0);
 }
